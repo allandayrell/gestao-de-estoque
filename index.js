ddfa86364c9326b3aebccc5ac9de9ad2dd5a053d
Allan Dayrell <allan.dayrell@gmail.com>
2024-05-12 22:15:26 -0300

    feat: interação com o usuário

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -9,24 +32,90 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const readCSV_1 = require("./model/readCSV");
 const controleEstoque_1 = require("./controller/controleEstoque");
+const controleEstoque_2 = require("./controller/controleEstoque");
+const controleEstoque_3 = require("./controller/controleEstoque");
+const prompt = require('prompt-sync')({ sigint: true });
+const readline = __importStar(require("readline"));
+const filePath = './model/estoque.csv';
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+const getInput = (question) => {
+    return new Promise((resolve) => {
+        rl.question(question, (answer) => {
+            resolve(answer);
+        });
+    });
+};
 const main = () => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const novoItem = {
+        /*
+        const novoItem: Data = {
             name: 'Uva',
             weight: 0.350,
             value: 3.5,
-            amount: 35
+            amount: 35,
+            status: 1
         };
-        yield (0, controleEstoque_1.adicionarProduto)(novoItem);
-        const data = yield (0, readCSV_1.readCSV)('./model/estoque.csv');
-        console.log('Dados lidos:', data);
-        //await writeCSV('./db/estoque.csv', data);
-        //console.log('Dados escritos em output.csv');
+        */
+        //
+        //let item = 'Laranja';
+        //await removerProduto(item, filePath); 
+        //const data = await readCSV(filePath);
+        //console.log('Dados lidos:', data);
+        //await listarProdutos(filePath);
+        console.log("Para adicionar produto, digite: 1");
+        console.log("Para remover produto, digite: 2");
+        console.log("Para lisar os produtos em estoque, digite: 3");
+        console.log("Para sair, digite: 4");
+        console.log("Digite sempre o nome com a primeira letra maiúscula e use ponto ao invés de virgula.");
+        let operacao = parseInt(yield getInput("Digite uma opção: "));
+        let primeiraOperacao = true;
+        while (operacao !== 4) {
+            if (!primeiraOperacao) {
+                operacao = parseInt(yield getInput("Digite uma opção: "));
+            }
+            switch (operacao) {
+                case 1:
+                    console.log("Digite as informações do Prouto:");
+                    const novoItem = {
+                        name: '',
+                        weight: 0,
+                        value: 0,
+                        amount: 0,
+                        status: 1
+                    };
+                    novoItem.name = yield getInput("Digite o nome do produto: ");
+                    novoItem.weight = parseFloat(yield getInput("Digite o peso do produto em gramas: "));
+                    novoItem.value = parseFloat(yield getInput("Digite o valor do produto: "));
+                    novoItem.amount = parseInt(yield getInput("Digite a quantidade do produto: "));
+                    yield (0, controleEstoque_1.adicionarProduto)(novoItem);
+                    break;
+                case 2:
+                    let item = yield getInput("Digite o nome do produto para ser removido: ");
+                    let certeza = parseInt(yield getInput("Tem certeza que deseja remover o item? se sim digite 1: "));
+                    if (certeza === 1) {
+                        yield (0, controleEstoque_2.removerProduto)(item, filePath);
+                    }
+                    break;
+                case 3:
+                    yield (0, controleEstoque_3.listarProdutos)(filePath);
+                    break;
+                case 4:
+                    operacao = 0;
+                    break;
+                default:
+                    console.log("Operação Inválida!");
+                    break;
+            }
+            primeiraOperacao = false;
+        }
     }
     catch (error) {
         console.error('Erro:', error);
     }
+    return 1;
 });
 main();
--- index.ts
+++ index.ts
@@ -1,29 +1,87 @@
-import {readCSV } from './model/readCSV';
-import { writeCSV } from './model/writeCSV';
 import { Data }  from "./model/interfaceData";
 import { adicionarProduto } from './controller/controleEstoque';
+import { removerProduto } from './controller/controleEstoque';
+import { listarProdutos } from './controller/controleEstoque';
+const prompt = require('prompt-sync')({sigint: true});
+import * as readline from 'readline';
+
+const filePath = './model/estoque.csv';
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const getInput = (question: string): Promise<string> => {
+    return new Promise((resolve) => {
+        rl.question(question, (answer) => {
+            resolve(answer);
+        });
+    });
+};
 
 const main = async () => {
     try {
-        const novoItem: Data = {
-            name: 'Uva',
-            weight: 0.350,
-            value: 3.5,
-            amount: 35
-        };
 
-        await adicionarProduto(novoItem);
+        console.log("Para adicionar produto, digite: 1");
+        console.log("Para remover produto, digite: 2");
+        console.log("Para lisar os produtos em estoque, digite: 3");
+        console.log("Para sair, digite: 4");
+        console.log("Digite sempre o nome com a primeira letra maiúscula e use ponto ao invés de virgula.")
 
+        let operacao: number = parseInt(await getInput("Digite uma opção: "));
+        let primeiraOperacao = true;
 
-        const data = await readCSV('./model/estoque.csv');
-        console.log('Dados lidos:', data);
+        while(operacao !== 4){
 
-        //await writeCSV('./db/estoque.csv', data);
-        //console.log('Dados escritos em output.csv');
+            if(!primeiraOperacao){
+                operacao = parseInt(await getInput("Digite uma opção: "));
+            }
+
+            switch (operacao) {
+                case 1:
+                    console.log("Digite as informações do Prouto:");
+                    const novoItem: Data = {
+                        name: '',
+                        weight: 0,
+                        value: 0,
+                        amount: 0,
+                        status: 1
+                    };
+                    
+                    novoItem.name = await getInput("Digite o nome do produto: ");
+                    novoItem.weight = parseFloat(await getInput("Digite o peso do produto em gramas: "));
+                    novoItem.value = parseFloat(await getInput("Digite o valor do produto: "));
+                    novoItem.amount = parseInt(await getInput("Digite a quantidade do produto: "));
+                    await adicionarProduto(novoItem);
+
+                    break;
+                case 2:
+                    let item = await getInput("Digite o nome do produto para ser removido: ");
+                    let certeza = parseInt(await getInput("Tem certeza que deseja remover o item? se sim digite 1: "));
+                    if(certeza === 1){
+                        await removerProduto(item, filePath);
+                    }
+                    break;
+                case 3: 
+                    await listarProdutos(filePath);
+                    break;
+                case 4:
+                    operacao = 0;
+                    break;
+                default:
+                    console.log("Operação Inválida!");
+                    break;
+            }
+
+            primeiraOperacao = false;
+        }
 
     } catch (error) {
         console.error('Erro:', error);
     }
+
+    return 1;
 };
 
 main();
